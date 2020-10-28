c865527afe24468c54f3e7645aaac9bb834452e0
Thedrummonger <drummerxtmail@gmail.com>
2020-10-27 21:54:53 -0500

    Disable the "import spoiler log" button if a game is loaded that does not have a spoiler log converter.

--- Other Games/SpoilerLogConverter.cs
+++ Other Games/SpoilerLogConverter.cs
@@ -19,6 +19,8 @@ namespace MMR_Tracker.Forms
 {
     public partial class SpoilerLogConverter : Form
     {
+
+        public static string[] SpoilerLogConvertable = new string[] { "MMR", "SSR", "OOTR", "WWR" };
         public SpoilerLogConverter()
         {
             InitializeComponent();
